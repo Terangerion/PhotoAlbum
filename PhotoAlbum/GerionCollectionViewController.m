4a1b07f84f0fd7740bc4653a71a2d1ab55265c5d
user1 <user1@LAB-N279.local>
2013-11-20 17:16:36 +0900

    セル内部の返却をAssetsLibraryを使用しないよう変更

--- PhotoAlbum/GerionCollectionViewController.m
+++ PhotoAlbum/GerionCollectionViewController.m
@@ -64,7 +64,6 @@
 
 // セクション数の返答用メソッド
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    NSLog(@"section in collection");
     return 1;
 }
 
@@ -76,30 +75,8 @@
 // セル内容の返答用メソッド
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     GerionCollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
-    
-#warning スクロールしようとしても画面がロックされてしまいます
-// ヒント：cellForItemAtIndexPathは何度も呼ばれるので、ここでenumerateGroupsWithTypes:usingBlock:を実行すると重くなります
-    
-    // AssetsLibraryでカメラロール情報を取得
-    // ALAssetPropertyDateのDESCでソートしたい
-    ALAssetsLibrary *library_ = [[ALAssetsLibrary alloc] init];
-    ALAssetsFilter *filter = [ALAssetsFilter allPhotos];
-    [library_ enumerateGroupsWithTypes:ALAssetsGroupAll usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
-        if (group) {
-            [group setAssetsFilter:filter];
-            NSMutableArray *assetList = [NSMutableArray array];
-            ALAssetsGroupEnumerationResultsBlock assetsEnumerationBlock = ^(ALAsset *asset, NSUInteger index, BOOL *stop) {
-                if (asset) {
-                    [assetList addObject:asset];
-                }else{
-                    UIImage *assetImage = [UIImage imageWithCGImage:[assetList[ [indexPath row] ] aspectRatioThumbnail]];
-                    cell.imageView.image = assetImage;
-                }
-            };
-            //アルバム(group)からALAssetの取得
-            [group enumerateAssetsUsingBlock:assetsEnumerationBlock];
-        }
-    } failureBlock:nil];
+    UIImage *assetImage = [UIImage imageWithCGImage:[self.assetList[ [indexPath row] ] aspectRatioThumbnail]];
+    cell.imageView.image = assetImage;
     return cell;
 }
 
